4e3e303688632a975b8d8cee487b698bc16da910
Thada Wangthammang <thada.wth@gmail.com>
2024-07-02 22:59:12 +0700

    add bff for swagger

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio-code
+
+- Generate OpenApi.json on build from ASP.NET 8 Minimal API
+https://gist.github.com/VincentH-Net/a950fb18363111ccac8e1fb64123647e
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +7,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("bff", new OpenApiInfo
+    {
+        Title = "BFF API",
+        Version = "v1",
+        Description = "Backend for Frontend API"
+    });
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "swagger/{documentName}/swagger.json";
+    });
+    // app.UseSwaggerUI();
+     app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/bff/swagger.json", "BFF API V1");
+        c.RoutePrefix = "swagger/bff";
+    });
 }
 
 app.UseHttpsRedirection();
